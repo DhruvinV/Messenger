a876901e9e846e5c2b61fc7a44e912e693e844ea
Dhruvin Vekariya <dhruvin.vekariya@mail.utoronto.ca>
2020-06-11 15:32:49 -0400

    Init Code

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Dhruvin Vekariya on 2020-06-11.
+//  Copyright © 2020 Neural Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
